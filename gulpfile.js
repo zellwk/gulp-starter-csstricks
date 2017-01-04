48e05ebc2ee07fbb9f8eeae5f52b826bd990a790
ted <tedgeving@users.noreply.github.com>
2017-01-04 08:53:56 -0700

    Update gulpfile.js
    Log gulp sass errors to console instead of exiting program.

--- gulpfile.js
+++ gulpfile.js
@@ -31,7 +31,7 @@ gulp.task('browserSync', function() {
 
 gulp.task('sass', function() {
   return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
-    .pipe(sass()) // Passes it through a gulp-sass
+    .pipe(sass().on('error', sass.logError)) // Passes it through a gulp-sass, log errors to console
     .pipe(gulp.dest('app/css')) // Outputs it in the css folder
     .pipe(browserSync.reload({ // Reloading with Browser Sync
       stream: true
